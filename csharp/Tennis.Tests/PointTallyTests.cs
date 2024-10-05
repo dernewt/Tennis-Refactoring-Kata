f7e9eb30719f295b607c9d8fa17dc0d7de1ca7ab
dernewt <dernewt@gmail.com>
2024-10-04 20:53:30 -0400

    first pass game8

---
+++ csharp/Tennis.Tests/PointTallyTests.cs
@@ -0,0 +1,61 @@
+﻿using Xunit;
+using FluentAssertions;
+using Tennis.Tennis8;
+
+namespace Tennis.Tests;
+
+public class PointTallyTests
+{
+    [Theory]
+    [InlineData(0, 0, true)]
+    [InlineData(3, 3, true)]
+    [InlineData(5, 5, true)]
+    [InlineData(0, 1, false)]
+    [InlineData(1, 0, false)]
+    [InlineData(3, 4, false)]
+    public void TiedGameTest(int score1, int score2, bool expected)
+    {
+        var game = new PointTally("a", "b", score1, score2);
+        game.IsTied().Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(5, 5, false)]
+    [InlineData(0, 3, false)]
+    [InlineData(2, 3, false)]
+    [InlineData(4, 5, true)]
+    [InlineData(0, 4, true)]
+    public void EndGameTest(int score1, int score2, bool expected)
+    {
+        var game = new PointTally("a", "b", score1, score2);
+        game.IsEndGame().Should().Be(expected);
+    }
+
+    [Fact]
+    public void AwardMissingPlayerFails()
+    {
+        var noBob = new PointTally("mike", "joe");
+
+        noBob.Invoking(p => p.Award("bob"))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void DuplicatePlayerFails()
+    {
+        var dupePlayer = () => new PointTally("", "");
+        dupePlayer.Should().Throw<ArgumentException>();
+    }
+
+    [Theory]
+    [ClassData(typeof(TestDataGenerator))]
+    public void AcceptanceTest(int points1, int points2, string expected)
+    {
+        var game = new PointTally(
+            TestDataGenerator.Player1Name,
+            TestDataGenerator.Player2Name,
+            points1, points2);
+
+        game.ToString(PointTally.GameFormat).Should().Be(expected);
+    }
+}
---
+++ csharp/Tennis/Tennis8/PointTally.cs
@@ -0,0 +1,102 @@
+﻿namespace Tennis.Tennis8;
+
+public class PointTally : IFormattable
+{
+    private readonly Dictionary<string, int> PointsPerPlayer;
+
+    public PointTally(string player1Name, string player2Name)
+        : this(player1Name, player2Name, Love, Love) { }
+
+    public PointTally(
+    string player1Name,
+    string player2Name,
+    int player1StartingPoints,
+    int player2StartingPoints)
+    {
+        if (player1Name == player2Name)
+            throw new ArgumentException("Players not unique");
+
+        PointsPerPlayer = new()
+    {
+        { player1Name, player1StartingPoints},
+        { player2Name, player2StartingPoints}
+    };
+    }
+
+    public bool IsTied() => PointsPerPlayer.Values.Distinct().Count() == 1;
+
+    public bool IsEndGame() => PointsPerPlayer.Values.Max() > Forty && !IsTied();
+
+    public void Award(string playerName)
+    {
+        try
+        {
+            PointsPerPlayer[playerName]++;
+        }
+        catch (KeyNotFoundException)
+        {
+            throw new ArgumentException("Invalid player");
+        }
+    }
+
+    public override string ToString() => ToString(DetermineFormat());
+
+    public const string EndGameFormat = "E";
+    public const string TiedFormat = "T";
+    public const string GameFormat = "A";
+    public const string DashedFormat = "D";
+    public string ToString(string? format, IFormatProvider? formatProvider = null)
+        => format switch
+        {
+            _ when formatProvider is not null => throw new NotImplementedException(),
+            DashedFormat or null or "" => ToStringWhenDashed(),
+            GameFormat => ToString(DetermineFormat()),
+            TiedFormat => ToStringWhenTied(),
+            EndGameFormat => ToStringWhenEndGame(),
+            _ => throw new FormatException()
+        };
+
+    protected const int NumberToWinBy = 2;
+
+    protected const int Love = 0;
+    protected const int Fifteen = 1;
+    protected const int Thirty = 2;
+    protected const int Forty = 3;
+
+    protected string DetermineFormat() => this switch
+    {
+        _ when IsTied() => TiedFormat,
+        _ when IsEndGame() => EndGameFormat,
+        _ => string.Empty,
+    };
+
+    public string ToStringWhenDashed() => string.Join("-",
+            PointsPerPlayer.Values.Select(ToStringSingular));
+
+    protected string ToStringWhenTied() => PointsPerPlayer.Values.First() switch
+    {
+        >= Forty => "Deuce",
+        var s => $"{ToStringSingular(s)}-All",
+    };
+
+    protected string ToStringWhenEndGame()
+    {
+        var orderedScores = PointsPerPlayer.OrderByDescending(s => s.Value);
+
+        if (orderedScores.First().Value - orderedScores.Last().Value >= NumberToWinBy)
+            return $"Win for {orderedScores.First().Key}";
+
+        return $"Advantage {orderedScores.First().Key}";
+    }
+
+    protected static string ToStringSingular(int score)
+    {
+        return score switch
+        {
+            Love => nameof(Love),
+            Fifteen => nameof(Fifteen),
+            Thirty => nameof(Thirty),
+            _ => nameof(Forty),
+        };
+    }
+}
